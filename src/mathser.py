8388a625d64d383b416ad1c313d3639a5b445d6a
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2023-01-01 19:46:00 +0100

    update logic

--- src/mathser.py
+++ src/mathser.py
@@ -3,22 +3,40 @@
 DELIMITER = "=========="
 
 
-def get_questions():
+def get_questions() -> list[str]:
     """Get questions from a source file.
 
     Returns:
-        The content of the source file
+        The content of the source file as a list.
     """
     with open("assets/questions.txt", "r") as file:
         content = file.read()
-    return content
+    return list(
+        filter(
+            lambda question: question not in ["", "\n"],
+            content.split(DELIMITER),
+        )
+    )
+
+
+def parse_questions(questions: list[str]) -> dict[str, str]:
+    """Parse questions from a string.
+
+    Args:
+        questions: The string containing the questions
+
+    Returns:
+        A list of questions
+    """
+    parsed_questions = {}
+    return parsed_questions
 
 
 if __name__ == "__main__":
     # get the content
     content = get_questions()
     # split the content into questions
-    questions = content.split(DELIMITER)
+    parsed_questions = parse_questions(content)
     # print the questions
-    for question in questions:
+    for question in parsed_questions:
         print(question)
